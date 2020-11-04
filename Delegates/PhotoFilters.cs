ea36ae108618a24ab243b6007549e9cf44e2e016
Cristian Nasirabadi <cristian.tirdad@gmail.com>
2020-11-04 03:49:11 +0100

    first commit

---
+++ Delegates/PhotoFilters.cs
@@ -0,0 +1,20 @@
+﻿namespace Delegates
+{
+    public class PhotoFilters
+    {
+        public void ApplyBrightness(Photo phpto)
+        {
+            System.Console.WriteLine("Apply brightness");
+        }
+
+        public void ApplyContrast(Photo phpto)
+        {
+            System.Console.WriteLine("Apply contrast");
+        }
+        public void Resize(Photo phpto)
+        {
+            System.Console.WriteLine("Resoze Photo");
+        }
+
+    }
+}
